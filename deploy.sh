67aeadb7c81a249bb76a64775960d58266f9c0a7
RDPStudio <67402215+rdp-studio@users.noreply.github.com>
2022-04-25 19:58:33 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
-git push -f git@github.com:chanshiyucx/chanshiyucx.github.io.git master
+git push -f git@github.com:rdp-studio/aurora.git gh-pages
 
 cd -
